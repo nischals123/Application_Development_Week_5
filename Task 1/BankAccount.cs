70610311fa78d886ef9f51a519a01669aa9b0454
Nischal <nischals054@gmail.com>
2025-11-25 11:45:28 +0545

    Task1

---
+++ Task 1/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿public class BankAccount
+{
+    // Private fields
+    private string accountNumber;
+    private double balance;
+
+    // Public property (only get)
+    public string AccountNumber
+    {
+        get { return accountNumber; }
+    }
+
+    // Public property for Balance (get only, set private)
+    public double Balance
+    {
+        get { return balance; }
+        private set
+        {
+            if (value <= 0)
+            {
+                Console.WriteLine("Balance cannot be negative.");
+            }
+            else
+            {
+                balance = value;
+            }
+        }
+    }
+
+    // Constructor
+    public BankAccount(string accNum, double openingBalance)
+    {
+        accountNumber = accNum;
+        Balance = openingBalance;   // calls the private setter
+    }
+
+    // Deposit method
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Deposit amount should be greater than 0.");
+        }
+        else
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited: {amount}");
+        }
+    }
+
+    // Withdraw method
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Withdraw amount must be greater than 0.");
+        }
+        else if (amount > balance)
+        {
+            Console.WriteLine("Insufficient balance!");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine($"Withdrawn: {amount}");
+        }
+    }
+}
---
+++ Task 1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating bank account object
+        BankAccount account = new BankAccount("ACCOUNT123", 4000);
+
+        // Display account number
+        Console.WriteLine("Account Number: " + account.AccountNumber);
+
+        // Deposit money
+        account.Deposit(2000);
+
+        // Withdraw money
+        account.Withdraw(1500);
+
+        // Print remaining balance
+        Console.WriteLine("Remaining Balance: " + account.Balance);
+    }
+}
